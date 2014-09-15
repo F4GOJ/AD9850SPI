62366975fcdbb420ecba1e02c194af5c361d91ad
Christophe Caiveau <f4goj@free.fr>
2014-09-15 04:28:46 +0200

    Update AD9850SPI.h

--- AD9850SPI.h
+++ AD9850SPI.h
@@ -16,15 +16,12 @@
  * DATA/D7 -> D11 arduino UNO/NANO, D51 MEGA
  * RESET   -> any pin except 10 and 12 UNO/NANO, 50 and 53 MEGA
  *
- * Instance :
- * AD9850SPI dds(W_CLK pin, FQ_UD pin, RESET pin);
- *
  * Functions :
- * dds.begin(); Initialize the output pins and master reset the AD9850 
- * dds.calibrate(frequency); Compensation of crystal oscillator frequency
- * dds.setfreq(frequency,phase); frequency in Hz, phase coded on 5 bits
- * dds.down(); power down mode reducing the dissipated power from 380mW to 30mW @5V
- * dds.up(); wake-up the AD9850
+ * DDS.begin(); Initialize the output pins and master reset the AD9850 
+ * DDS.calibrate(frequency); Compensation of crystal oscillator frequency
+ * DDS.setfreq(frequency,phase); frequency in Hz, phase coded on 5 bits
+ * DDS.down(); power down mode reducing the dissipated power from 380mW to 30mW @5V
+ * DDS.up(); wake-up the AD9850
  *
  * AD9850 datasheet at http://www.analog.com/static/imported-files/data_sheets/AD9850.pdf
  *
@@ -41,10 +38,10 @@ class AD9850SPI
   	AD9850SPI();
 
 	void begin(int w_clk, int fq_ud, int reset);
-	void setfreq(double f, uint8_t p);
+	void setfreq(long f, uint8_t p);
 	void down();
 	void up();
-	void calibrate(double TrimFreq);
+	void calibrate(long TrimFreq);
 	
 	private:
 	int W_CLK;
@@ -55,7 +52,7 @@ class AD9850SPI
 	void update();
 	void begin_priv();
 	void pulse(int pin);
-	double calibFreq;
+	long calibFreq;
 };
 
 extern AD9850SPI DDS;
